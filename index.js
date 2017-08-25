e341de709478ec03d8da5022d1f720e96e8cb3a1
chen chao <chao.wings@gmail.com>
2017-08-26 00:28:01 +0800

    fix bug on firstname and lastname

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.get('/', function(request, response) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1,"firstname":"Bob","lastname":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstname":"Tammy","lastname":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstname":"Tina","lastname":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
 
